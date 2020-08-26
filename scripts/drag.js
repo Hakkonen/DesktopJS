a760619207ded48f96f940391027c63d19e8834a
hakkonen <rnlds.j@gmail.com>
2020-08-26 21:35:41 +1000

    Window movement and toolbar created

---
+++ scripts/drag.js
@@ -0,0 +1,37 @@
+export function dragElement(element) {
+    // 1. Init x/y coords
+    let pos1 = 0, pos2 = 0, pos3 = 0
+    
+    // Make the whole element draggable
+    element.onmousedown = dragMouseDown
+
+    function dragMouseDown(e) {
+        e = e || window.event
+        e.preventDefault()
+        // Get mouse position on click
+        pos3 = e.clientX
+        pos4 = e.clientY
+        document.onmouseup = closeDragElement
+        // Call drag function whenever mouse moves
+        document.onmousemove = elementDrag
+    }
+
+    function elementDrag(e) {
+        e = e || window.event
+        e.preventDefault()
+        // Calculate new cursor position
+        pos1 = pos3 - e.clientX 
+        pos2 = pos4 - e.clientY
+        pos3 = e.clientX
+        pos4 = e.clientY
+        // Set the element's new positon
+        element.style.top = (element.offsetTop - pos2) + "px"
+        element.style.left = (element.offsetLeft - pos1) + "px"
+        console.log(pos1, pos2)
+    }
+
+    function closeDragElement() {
+        document.onmouseup = null
+        document.onmousemove = null
+    }
+}
